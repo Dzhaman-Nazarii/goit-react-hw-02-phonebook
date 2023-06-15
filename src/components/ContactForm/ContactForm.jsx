7e29779756e57460dd16746bafc3b16775f93e38
Dzhaman-Nazarii <dzhaman.nazar2003@gmail.com>
2023-06-15 18:09:44 +0300

    change pattern

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -42,8 +42,8 @@ export default class ContactForm extends Component {
                     name="name"
                     value={name}
                     onChange={this.handleChange}
-                    pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
-                    title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+                    pattern="^[a-zA-Zа-яА-Я]+(([' \-][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$" 
+                    title="Name may contain only  letters, spaces, hyphens, and apostrophes are allowed"
                     required
                 />
                 <label htmlFor="this.numberId"></label>
@@ -55,8 +55,8 @@ export default class ContactForm extends Component {
                     name="number"
                     value={number}
                     onChange={this.handleChange}
-                    pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
-                    title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+                    pattern="\+?\d{1,4}?[ .\-\s]?\(?\d{1,3}?\)?[ .\-\s]?\d{1,4}[ .\-\s]?\d{1,4}[ .\-\s]?\d{1,9}"
+                    title="Valid Phone Number: Optional '+' Symbol, Digits, Spaces, Hyphens, and Parentheses"
                     required
                 />
                 <button type="submit" className={css.formButton}>Add contact</button>
